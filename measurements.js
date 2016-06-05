01b61f71c10351d867ff01c6964732ab6c62cf53
Konstantin Cherkashin <che.constantine@gmail.com>
2016-06-05 15:57:27 +0300

    init

---
+++ measurements.js
@@ -0,0 +1,158 @@
+var dryUnits = [
+  {unit: "gram", minimum: 1, divisible: 1},
+  {unit: "tsp", minimum: 0, divisible: 0.25},
+  {unit: "tbsp", minimum: 1, divisible: 0.5},
+  {unit: "ounce", minimum: 1, divisible: 1},
+  {unit: "cup", minimum: 1, divisible: 0.5}
+];
+
+var weightUnits = [
+  {unit: "gram", minimum: 1, divisible: 1},
+  {unit: "tsp", minimum: 0, divisible: 0.25},
+  {unit: "tbsp", minimum: 1, divisible: 0.5},
+  {unit: "ounce", minimum: 0.25, divisible: 0.25},
+  {unit: "pound", minimum: 1, divisible: 0.25}
+];
+
+var liquidUnits = [
+  {unit: "tsp", minimum: 0, divisible: 0.25},
+  {unit: "tbsp", minimum: 1, divisible: 0.5},
+  {unit: "ounce", minimum: 1, divisible: 1},
+  {unit: "cup", minimum: 1, divisible: 0.5},
+  {unit: "liter", minimum: 1, divisible: 0.5},
+  {unit: "gallon", minimum: 1, divisible: 1}
+];
+
+// Here are the grams/unit
+var rates = {
+  "gram": 1,
+  "tsp": 5,
+  "tbsp": 15,
+  "ounce": 28.35,
+  "cup": 226.8,
+  "pound": 453.6,
+  "liter": 1000,
+  "gallon": 3520
+};
+
+
+function getKind(kind)
+{
+    switch (kind) {
+    case "dry": return dryUnits;
+    case "weight": return weightUnits;
+    case "volume": return liquidUnits;
+    }
+    throw new Error("Unexpected units kind: " + kind);
+}
+
+
+function division(unit, qty, kind)
+{
+    var divisible = null;
+    var formatDivision = function(qty, divis) {
+        if (0 != qty / divis % 1) {
+            throw new Error("Unexpected fraction: " + qty + " " + unit + " can be divided by " + divis);
+        }
+        if (0 == (qty / divis) % 2 && 0 == (1 / divis) % 2) {
+            divis = divis * 2;
+        }
+        return qty / divis + "/" + 1 / divis;
+    };
+    getKind(kind).forEach(function(measure) {
+        if (measure.unit == unit) {
+            divisible = measure.divisible;
+        }
+    });
+    if (1 > qty) {
+        return formatDivision(qty, divisible);
+    }
+    if (1 < qty) {
+        rem = qty % 1;
+        return (qty - rem) + " " + formatDivision(rem, divisible);
+    }
+}
+
+function pluralize(unit, qty, kind)
+{
+    var article = ("ounce" == unit) ? "an" : "a";
+    if (qty < 1 && 0 < qty) {
+        return division(unit, qty, kind) + " of " + article + " " + unit;
+    }
+    if (1 == qty) {
+        return article + " " +  unit;
+    }
+    if (1 < qty && 0 != qty % 1) {
+        return division(unit, qty, kind) + " " + unit + "s";
+    }
+    if (1 < qty && 0 == qty % 1) {
+        return qty + " " + unit + "s";
+    }
+    throw new Error("Unexpected qty");
+}
+
+function generate(qty, kind)
+{
+    var unit = getUnit(qty, kind);
+    var unitQty = round(qty / rates[unit.unit], unit);
+//    console.log(unitQty);
+    return pluralize(unit.unit, unitQty, kind);
+}
+
+function round(qty, unit)
+{
+//    console.log(qty);
+    divisible = unit.divisible;
+    quot = qty - qty % 1;
+    rem = qty % 1;
+    roundedRem = Math.round(rem / divisible) * divisible;
+    return quot + roundedRem - roundedRem % divisible;
+}
+
+
+function approximate(qty, test, divisible)
+{
+    if (1 == divisible) {
+        var divisors = [1];
+    } else {
+        var divisors = (0.25 == divisible) ? [1, 0.25, 0.5, 0.75] : [1, 0.5];
+    }
+//    console.log(divisible);
+    var approximation = null;
+    divisors.forEach(function (divisor) {
+        var proportion = test * divisor / qty;
+//        console.log(proportion + " test " + test + " div" + divisor);
+        if (proportion <= 1.1 && proportion >= 0.9) {
+            approximation = test * divisor;
+        }
+    });
+    return approximation;
+}
+
+
+function getUnit (qty, kind)
+{
+    var measure = null;
+    getKind(kind).forEach(function(unit) {
+        var rate = rates[unit.unit];
+//        console.log(unit);
+        var approximation = approximate(qty, rate, unit.divisible);
+        if (approximation && rate / approximation >= unit.minimum) {
+            measure = unit;
+        } else { if (qty >= rate) {
+            measure = unit;
+        }}
+    });
+    if (measure) {
+        return measure;
+    }
+    throw new Error("Unexpected qty");
+}
+
+exports.pluralize = pluralize;
+exports.getUnit = getUnit;
+exports.division = division;
+exports.approximate = approximate;
+exports.generate = generate;
+
+//console.log(generate(120, "dry"));
---
+++ test/measurements.js
@@ -0,0 +1,50 @@
+var measurements = require("../measurements");
+
+exports.units = function(test) {
+    test.equal(measurements.getUnit(3, "dry").unit, "gram");
+    test.equal(measurements.getUnit(1, "dry").unit, "gram");
+    test.equal(measurements.getUnit(5, "dry").unit, "tsp");
+    test.equal(measurements.getUnit(9, "dry").unit, "tsp");
+    test.equal(measurements.getUnit(110, "dry").unit, "cup");
+    test.equal(measurements.getUnit(200, "dry").unit, "ounce");
+    test.equal(measurements.getUnit(216, "dry").unit, "cup");
+    test.equal(measurements.getUnit(1666, "dry").unit, "cup");
+    test.equal(measurements.getUnit(1666, "volume").unit, "liter");
+    test.throws(function () { measurements.getUnit(-15, "volume");}, Error);
+    test.done();
+}
+
+exports.plurality = function(test) {
+    test.equal(measurements.pluralize("gram", 1, "dry"), "a gram");
+    test.equal(measurements.pluralize("ounce", 10, "dry"), "10 ounces");
+    test.equal(measurements.pluralize("tsp", 0.25, "dry"), "1/4 of a tsp");
+    test.equal(measurements.pluralize("cup", 1.5, "dry"), "1 1/2 cups");
+    test.throws(function () { measurements.pluralize("ounce", -1, "volume");}, Error);
+    test.done();
+}
+
+exports.division = function(test) {
+    test.equal(measurements.division("tsp", 0.25, "volume"), "1/4");
+    test.equal(measurements.division("tsp", 1.25, "volume"), "1 1/4");
+    test.equal(measurements.division("tsp", 1.5, "volume"), "1 1/2");
+    test.throws(function () { measurements.division("tsp", 0.3, "dry");}, Error);
+    test.throws(function () { measurements.division("tbsp", 0.25, "volume");}, Error);
+    test.throws(function () { measurements.division("ounce", 1.5, "volume");}, Error);
+    test.done();
+}
+
+exports.approximation = function(test) {
+    test.ok(measurements.approximate(214, 226.8, 0.5));
+    test.ok(!measurements.approximate(200, 226.8, 0.5));
+    test.ok(measurements.approximate(214, 226.8, 0.5));
+    test.ok(measurements.approximate(1.2, 5, 0.25));
+    test.done();
+}
+
+exports.generate = function(test) {
+    test.equal(measurements.generate(200, "dry"), "7 ounces");
+    test.equal(measurements.generate(220, "dry"), "a cup");
+    test.equal(measurements.generate(110, "dry"), "1/2 of a cup");
+    test.equal(measurements.generate(29, "dry"), "an ounce");
+    test.done();
+}
